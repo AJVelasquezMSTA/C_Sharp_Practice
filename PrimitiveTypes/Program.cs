570d371a845c24168171f24923c07fc217c772e6
Ashley Velasquez <56100421+AJVelasquezMSTA@users.noreply.github.com>
2020-03-15 18:28:02 -0700

    Add Primitive Types Project
    Project exploring if primitive types can be nullable. Short answer: no.

---
+++ PrimitiveTypes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PrimitiveTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Types test = new Types();
+            test.num1 = 3;
+            bool result = test.num1.HasValue;
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+    public class Types
+    {
+        public int? num1;
+        public static Types Spaget => true;
+    }
+}
